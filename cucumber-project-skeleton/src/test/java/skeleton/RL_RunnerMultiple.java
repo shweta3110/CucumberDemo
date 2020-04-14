fff2b4a7b422b22b193cd5f31631eef5785d6def
Sakshi <Sakshi@DESKTOP-0H07TE8>
2020-04-14 12:35:53 +0530

    nhj

--- cucumber-project-skeleton/src/test/java/skeleton/RL_RunnerMultiple.java
+++ cucumber-project-skeleton/src/test/java/skeleton/RL_RunnerMultiple.java
@@ -3,8 +3,6 @@ package skeleton;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
