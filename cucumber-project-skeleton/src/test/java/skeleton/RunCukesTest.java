fd8c6bfcaeaebe953d5356023156c27a89026716
Sakshi <Sakshi@DESKTOP-0H07TE8>
2020-04-14 12:28:12 +0530

    fgbfgbd

--- cucumber-project-skeleton/src/test/java/skeleton/RunCukesTest.java
+++ cucumber-project-skeleton/src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( features= {"classpath:skeleton"},glue= {"classpath:skeleton"},plugin={ "pretty","json:C:/Test_Report/cucumber.json"}, tags= { "@SanityTest" })
+@CucumberOptions(features = { "classpath:skeleton" }, glue = { "classpath:skeleton" }, plugin = { "pretty",
+		"json:C:/Test_Report/cucumber.json" }, tags = { "@DemoWebShop" })
 public class RunCukesTest {
 }
